dafb5dc2d30dfaf320e8688aa6a4ccfbde7191f9
Matheus_code <contatomh11@gmail.com>
2026-08-07 17:17:55 -0300

    config: configurando vite e deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/society-scheduler/',
-  plugins: [vue()],
+  base: '/society-scheduler/', // <-- Está perfeito!
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
